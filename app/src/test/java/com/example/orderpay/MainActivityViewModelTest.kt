a711fd337b792db8d54fc065241c7bf2ae9809de
rburugula <burugular@gmail.com>
2021-09-06 14:23:19 +0100

    Updated viewmodel test removed code which is not necessary

--- app/src/test/java/com/example/orderpay/MainActivityViewModelTest.kt
+++ app/src/test/java/com/example/orderpay/MainActivityViewModelTest.kt
@@ -1,8 +1,6 @@
 package com.example.orderpay
 
-import androidx.lifecycle.Observer
 import com.example.orderpay.data.DataRepository
-import com.example.orderpay.model.Character
 import com.example.orderpay.viewModel.MainActivityViewModel
 import com.nhaarman.mockitokotlin2.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -11,16 +9,12 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class MainActivityViewModelTest {
     @Mock
     private lateinit var dataRepository: DataRepository
-
-    @Mock
-    private lateinit var dataObserver: Observer<Result<List<Character>>>
 
     @InjectMocks
     private lateinit var mainActivityViewModel: MainActivityViewModel
