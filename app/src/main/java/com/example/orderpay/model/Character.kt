cb46d5e94ea749507c20d3005e96473325287788
rburugula <burugular@gmail.com>
2021-09-06 13:00:06 +0100

    Modified the Character object to retrieve what we use in the app

--- app/src/main/java/com/example/orderpay/model/Character.kt
+++ app/src/main/java/com/example/orderpay/model/Character.kt
@@ -7,8 +7,6 @@ import kotlinx.parcelize.Parcelize
 data class Character(
     val name: String,
     val img: String,
-    val occupation: ArrayList<String>,
     val status: String,
     val nickname: String,
-    val appearance: ArrayList<Int>
 ) : Parcelable
